b7d68d2adca29adf9b452562abfc99957ee99ed4
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-08-20 09:40:56 +0200

    bug fixed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,5 @@
 import mapboxgl from 'mapbox-gl';
-import { removePopups, addPopupsInBox, addMarkersToMap } from './manage_map_elements'
+import { addPopupsInBox, addMarkersToMap, managePopups } from './manage_map_elements'
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -12,21 +12,18 @@ const initMapbox = () => {
       center: [2.3454, 48.8549], // starting position [lng, lat]
       zoom: 15 // starting zoom
     });
+
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(markers, map);
     addPopupsInBox(markers, map);
+
     map.on('moveend', function() {
-      const zoomLevel = map.getZoom();
-      if (zoomLevel < 14.5) {
-        removePopups();
-        addMarkersToMap(markers, map);
-      } else {
-        addPopupsInBox(markers, map);
-        addMarkersToMap(markers, map);
-        $('#hour-input').on('change', function () {
-          addPopupsInBox(markers, map);
-        });
-      }
+      addMarkersToMap(markers, map);
+      managePopups(markers, map);
+    });
+
+    $('#hour-input').on('change', function () {
+      managePopups(markers, map);
     });
   }
 };
