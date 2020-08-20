d688f0204805dfce07abb8b8532bc46798a1f35b
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-08-20 08:31:33 +0200

    markers only loading when needed

--- app/javascript/plugins/generate_html.js
+++ app/javascript/plugins/generate_html.js
@@ -1,14 +1,14 @@
-const createMarker = (element) => {
+const generateMarkerHtml = (element) => {
   element.className = 'marker';
   element.style.backgroundColor = "#59B0E3";
-  element.style.width = "3px";
-  element.style.height = "3px";
+  element.style.width = "5px";
+  element.style.height = "5px";
   element.style.borderRadius = "50%";
   element.style.fontSize = '0.5rem';
 };
 
-const createPopup = (number1, number2) => {
+const generatePopupHtml = (number1, number2) => {
   return `<p>${number1} / ${number2}</p>`
 };
 
-export { createMarker, createPopup }
+export { generateMarkerHtml, generatePopupHtml }
---
+++ app/javascript/plugins/manage_map_elements.js
@@ -0,0 +1,65 @@
+import mapboxgl from 'mapbox-gl';
+import { generateMarkerHtml, generatePopupHtml } from './generate_html'
+import { matchHours } from './match_hours'
+import { getUserInput } from './init_select2'
+
+const createAPopup = (marker, map) => {
+  const userInput = getUserInput();
+  const userInputTranslated = matchHours[`${userInput}`]
+  const popup = new mapboxgl.Popup({ closeOnClick: false })
+  .setLngLat([ marker.lon, marker.lat ])
+  .setHTML(generatePopupHtml(marker[`current_${userInputTranslated}`], marker["capacity"]))
+  .addTo(map);
+};
+
+const createAMarker = (marker, map) => {
+  const element = document.createElement('div');
+  generateMarkerHtml(element);
+  new mapboxgl.Marker(element)
+    .setLngLat([ marker.lon, marker.lat ])
+    .addTo(map);
+
+}
+
+const removePopups = () => {
+  const popups = document.querySelectorAll(".mapboxgl-popup");
+  popups.forEach((popup) => {
+    popup.remove();
+  });
+};
+
+const removeMarkers = () => {
+  const markers = document.querySelectorAll(".marker");
+  markers.forEach((marker) => {
+    marker.remove();
+  });
+};
+
+const addPopupsInBox = (markers, map) => {
+  removePopups();
+  const neLng = map.getBounds()["_ne"].lng;
+  const neLat = map.getBounds()["_ne"].lat;
+  const swLng = map.getBounds()["_sw"].lng;
+  const swLat = map.getBounds()["_sw"].lat;
+  markers.forEach((marker) => {
+    if ( swLng < marker.lon && marker.lon < neLng && marker.lat > swLat && marker.lat < neLat) {
+      createAPopup(marker, map);
+    };
+  });
+};
+
+const addMarkersToMap = (markers, map) => {
+  removeMarkers();
+  const neLng = map.getBounds()["_ne"].lng;
+  const neLat = map.getBounds()["_ne"].lat;
+  const swLng = map.getBounds()["_sw"].lng;
+  const swLat = map.getBounds()["_sw"].lat;
+  markers.forEach((marker) => {
+    if ( swLng < marker.lon && marker.lon < neLng && marker.lat > swLat && marker.lat < neLat) {
+      createAMarker(marker, map);
+    };
+  });
+};
+
+export { removePopups, addPopupsInBox, addMarkersToMap }
+  
